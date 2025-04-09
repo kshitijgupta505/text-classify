4c18e222913f133409547af58591f31a4d612c7a
KSHITIJ GUPTA <91895577+kshitijgupta505@users.noreply.github.com>
2025-04-09 14:46:57 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -11,15 +11,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-// kshitij505
-
-// next.config.js
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // ✅ Add this line
-  },
-};
-
-module.exports = nextConfig;
